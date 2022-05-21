3f032fb5363286e653873b039451dcb8f3b839d7
Michael Bonilla <mike.bonilla96@gmail.com>
2022-05-20 21:27:35 -0500

    Modified port

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express')
 const overrides = require("./overrides.json");
 
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static('public'));
 app.get("/", (req, res) => {
